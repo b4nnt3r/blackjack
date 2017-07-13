4a17c69e2cc6307d84df0a3b8d1c58f832a7207a
Brandon Walden <brandonshanewalden@gmail.com>
2017-07-13 16:02:02 -0400

    needs work

--- scripts/main.js
+++ scripts/main.js
@@ -5,18 +5,28 @@
    to watch the assertions pass as you write your code.
 
    Also remember, that the parameter `hand` will be an array, so
-   you'll need to parse through that first before you can start to 
+   you'll need to parse through that first before you can start to
    write your logic.
 */
+let hand = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
 
+function handValue(hand) {
+    return hand.reduce(function(s, v) {
+      return s + v;
+    });
 
-function handValue (hand) {
-
-
-  return;
 }
 
 
+
+
+
+// function setAll(a, v) {
+//     var i, n = a.length;
+//     for (i = 0; i < n; ++i) {
+//         a[i] = v;
+//     }
+// }
 /* -----  Hints ------
 
 1..10   ==> Worth face value (1 = 1, 4 = 4, etc)
